1e9db6b76c32bc324ce2de196461d11c5bca5f72
nizaelvalente <nizaelvalente@gmail.com>
2021-06-06 14:23:41 -0300

    s

---
+++ js.js
@@ -0,0 +1,10 @@
+
+function exibir() {
+    const resume = document.getElementById('resume')
+    if(resume.style.display = 'none' ){
+        resume.style.display = 'block'
+    } else{
+        resume.style.display = 'none'
+    }
+}
+
