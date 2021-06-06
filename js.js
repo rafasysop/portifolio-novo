0bff8704c96b91fcb7c19db0ac6035a04be239b3
Rafael Moura <rafaelcostamoura@gmail.com>
2021-06-06 16:08:53 -0300

    Adicionei os eventos abre e fecha e modifiquei CSS

--- js.js
+++ js.js
@@ -1,10 +1,21 @@
-const resume = document.getElementById('resume')
 
+const btnResume = document.querySelector('#li-resume')
+const btnClose= document.querySelector('.close')
+const resume = document.getElementById('resume')
 const exibir = () => {
-    resume.style.display = 'block'
+    // resume.style.display = 'block'
+    // resume.style.right = '5vw';
+    console.log(' exibe');
+    resume.classList.add('show-resume')
 }
 
 const fechar = () => {
-    resume.style.display = 'none'
+    // resume.style.display = 'none'
+    // resume.style.right = '-15vw';
+    console.log('fecha');
+    resume.classList.remove('show-resume')
 }
+btnResume.addEventListener("click", exibir)
+btnClose.addEventListener("click", fechar)
+
 
